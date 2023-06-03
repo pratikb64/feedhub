8d27d929699104665cfd771b25316521d733ad5c
Pratik <badhepd@gmail.com>
2023-06-03 16:02:16 +0530

    projects list on popup

---
+++ src/components/ProjectItem.tsx
@@ -0,0 +1,57 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { useState } from "react"
+import { BsThreeDots, BsTrash } from "react-icons/bs"
+
+const ProjectItem = ({ domain, id }: { domain: string; id: string }) => {
+  const [deleteClicked, setDeleteClicked] = useState(false)
+  const deleteHandler = () => {}
+
+  const openHandler = () => {}
+  return (
+    <div
+      onClick={openHandler}
+      className="m-2 flex cursor-pointer items-center justify-between rounded-md border-2 border-gray-700 p-2 transition-colors hover:bg-violet-800">
+      <div className="font-semibold">{domain}</div>
+      <div>
+        <DropdownMenu.Root
+          onOpenChange={() => {
+            setDeleteClicked(false)
+          }}>
+          <DropdownMenu.Trigger asChild>
+            <button
+              aria-label="Profile menu"
+              className="flex items-center gap-1">
+              <BsThreeDots size={22} />
+            </button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="w-32 rounded-md bg-slate-800 p-2 shadow-lg"
+              sideOffset={5}>
+              {deleteClicked ? (
+                <DropdownMenu.Item className="rounded-md outline-none hover:bg-slate-700">
+                  <button
+                    onClick={deleteHandler}
+                    className="flex w-full items-center gap-1 p-2 font-semibold text-red-500">
+                    Are you sure?
+                  </button>
+                </DropdownMenu.Item>
+              ) : (
+                <div className="rounded-md outline-none hover:bg-slate-700">
+                  <button
+                    onClick={() => setDeleteClicked(true)}
+                    className="flex w-full items-center gap-1  p-2 font-semibold text-red-500">
+                    <BsTrash size={18} />
+                    Delete
+                  </button>
+                </div>
+              )}
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectItem
--- src/popup/routes/Home.tsx
+++ src/popup/routes/Home.tsx
@@ -1,6 +1,8 @@
+import type { Models } from "appwrite"
 import { useEffect, useState } from "react"
 import { RiAddFill } from "react-icons/ri"
 import Layout from "~components/Layout"
+import ProjectItem from "~components/ProjectItem"
 import useCurrentTab from "~hooks/useCurrentTab"
 import useDatabase from "~hooks/useDatabase"
 import useFunctions from "~hooks/useFunctions"
@@ -10,12 +12,15 @@ const Home = () => {
   const [isLoading, setIsLoading] = useState(false)
   const { fn } = useFunctions()
   const { getDocumentList } = useDatabase()
+  const [projects, setProjects] =
+    useState<Models.DocumentList<Models.Document>>()
 
   const clickHandler = async () => {
     setIsLoading(true)
     await fn("create-project", {
       domain: data?.url
     })
+    await getProjects()
     setIsLoading(false)
   }
 
@@ -23,7 +28,7 @@ const Home = () => {
     const projects = await getDocumentList({
       collectionId: "projects"
     })
-    console.log(projects)
+    setProjects(projects)
   }
 
   useEffect(() => {
@@ -32,22 +37,38 @@ const Home = () => {
 
   return (
     <Layout>
-      <div className="mt-16 flex flex-col items-center justify-center">
-        <div className="text-xl font-semibold">
-          You don't have any project yet
-        </div>
-        <div className="mt-4 text-center text-sm text-gray-400">
-          Create a new project for{" "}
-          <b className="text-violet-500">{data?.url}</b> <br /> and get started
-        </div>
-        <div className="mt-12">
-          <button
-            onClick={clickHandler}
-            className="flex items-center rounded-md bg-violet-500 p-2 px-4 font-bold text-white hover:bg-violet-600 active:bg-violet-700"
-            disabled={isLoading}>
-            <RiAddFill size={24} />
-            Create Project
-          </button>
+      <div>
+        {projects?.total == 0 && (
+          <div className="mt-16 flex flex-col items-center justify-center">
+            <div className="text-xl font-semibold">
+              You don't have any project yet
+            </div>
+            <div className="mt-4 text-center text-sm text-gray-400">
+              Create a new project for{" "}
+              <b className="text-violet-500">{data?.url}</b> <br /> and get
+              started
+            </div>
+            <div className="mt-12">
+              <button
+                onClick={clickHandler}
+                className="disabled:bggr flex items-center rounded-md bg-violet-500 p-2 px-4 font-bold text-white hover:bg-violet-600 active:bg-violet-700 disabled:bg-gray-500"
+                disabled={isLoading}>
+                <RiAddFill size={24} />
+                Create Project
+              </button>
+            </div>
+          </div>
+        )}
+        <div>
+          {projects?.documents.map((project) => {
+            return (
+              <ProjectItem
+                domain={project.domain}
+                id={project.$id}
+                key={project.$id}
+              />
+            )
+          })}
         </div>
       </div>
     </Layout>
