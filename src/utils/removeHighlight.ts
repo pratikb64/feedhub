5791694cf368c348be4aa1683e49825ba8a9961a
Pratik <badhepd@gmail.com>
2023-06-04 17:47:50 +0530

    bug fix: elements moving on hover

--- src/utils/removeHighlight.ts
+++ src/utils/removeHighlight.ts
@@ -3,5 +3,5 @@ import type { HTMLElementEvent } from "./types"
 export default function removeHighlight(e: MouseEvent) {
   const event = e as HTMLElementEvent
   event.target.style.backgroundColor = ""
-  event.target.style.border = ""
+  event.target.style.boxShadow = ""
 }
--- src/utils/setHighlight.ts
+++ src/utils/setHighlight.ts
@@ -3,5 +3,5 @@ import type { HTMLElementEvent } from "./types"
 export default function setHighlight(e: MouseEvent) {
   const event = e as HTMLElementEvent
   event.target.style.backgroundColor = "rgba(51,153,255,0.6)"
-  event.target.style.border = "1px solid rgb(51,153,255)"
+  event.target.style.boxShadow = "rgb(51,153,255) 0px 0px 0px 1px"
 }
